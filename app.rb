98d98d92a74b8fc655eebfd099bb35e855a157e6
Zee Spencer <zee@zeespencer.com>
2014-06-14 20:14:21 -0700

    You can check some tweets!

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
+require 'json'
+
 require 'sinatra'
+require 'simple_oauth'
+require 'excon'
+begin
+require 'dotenv'
+Dotenv.load
+rescue
+end
 
 
 def reverse(words)
   words.split(" ").map do |word|
     word.reverse
   end.join(" ")
+end
+
+
+def tweets(screen_name)
+  authorization_header = SimpleOAuth::Header.new("get",
+                                                 "https://api.twitter.com/1.1/statuses/user_timeline.json",
+                                                 { :screen_name => screen_name },
+                                                 { :consumer_key => ENV['TWITTER_API_KEY'],
+                                                   :consumer_secret => ENV['TWITTER_API_SECRET'] })
+
+  response = Excon.send("get", "https://api.twitter.com/1.1/statuses/user_timeline.json", {
+    :query => { :screen_name => screen_name },
+    :headers => { "Authorization" => authorization_header.to_s }
+  })
+  JSON.parse(response.body)
 end
 
 QUOTES = ["It's life Jim, but not as we know it!",
@@ -16,7 +40,16 @@ get '/' do
 end
 
 post '/reverse' do
-  puts params
   @words = reverse(params[:words])
   erb :home
+end
+
+get '/tweets' do
+  if params[:screen_name]
+    @tweets = tweets(params[:screen_name])
+  else
+    @tweets = []
+  end
+
+  erb :tweets
 end
